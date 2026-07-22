0dcf3f13c2f1cd49f5de651722fe84d0610c1085
Manish Bhatt <52626736+bhattmanish98@users.noreply.github.com>
2026-07-22 13:27:07 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    class Pair {
+        int num;
+        int freq;
+
+        Pair(int num, int freq) {
+            this.num = num;
+            this.freq = freq;
+        }
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        // Frequency map or array? array will be better when alphabets are there. Here Map is better
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int i: nums) freqMap.put(i, freqMap.getOrDefault(i, 0) + 1);
+
+        // First K elements based of freq, Max heap (Priority Queue) is best to use
+        PriorityQueue<Pair> pq = new PriorityQueue<>((p1, p2) -> p1.freq-p2.freq);
+        for(int i: freqMap.keySet()) {
+            pq.offer(new Pair(i, freqMap.get(i)));
+            if (pq.size() > k) pq.poll();
+
+        }
+
+        int[] result = new int[k];
+        int i = 0;
+        while(k-->0) result[i++] = pq.poll().num;
+        return result;
+    }
+}
