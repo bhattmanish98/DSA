d8569ef6b03feb192900195db00ae9b1299d5a12
Manish Bhatt <52626736+bhattmanish98@users.noreply.github.com>
2026-07-22 13:02:08 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Maintain a map, key is sorted string and values are list of all strings which has same
+        // sorted order.
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String s_sorted = new String(c);
+            List<String> group = map.getOrDefault(s_sorted, new LinkedList<>());
+            group.add(s);
+            map.put(s_sorted, group);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        for (List<String> list : map.values()) result.add(list);
+        return result;
+    }
+}
